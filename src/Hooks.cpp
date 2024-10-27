20eb3264fb45ffa41a43d92d3c40ec31b7cc58ac
lambdacdm1 <ramonrules3@hotmail.com>
2024-10-27 16:13:17 -0700

    Initial commit

---
+++ src/Hooks.cpp
@@ -0,0 +1,13 @@
+#include "Hooks.h"
+
+namespace Hooks
+{
+    void Install() noexcept
+    {
+        stl::write_thunk_call<MainUpdate>();
+        logger::info("Installed main update hook");
+        logger::info("");
+    }
+
+    i32 MainUpdate::Thunk() noexcept { return func(); }
+} // namespace Hooks
